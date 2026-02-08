5345c5870f91640591a56d5aea08ed66a0cc56a3
Ajyarijyari <aostress1030@gmail.com>
2026-02-08 16:07:51 +0900

    add legal pages, GA4 loader, and 404

---
+++ ga.js
@@ -0,0 +1,14 @@
+﻿// GA4 loader (replace G-XXXXXXXXXX)
+(function(){
+  const id = "G-XXXXXXXXXX";
+  if(!id || id === "G-XXXXXXXXXX"){ return; }
+  const s1 = document.createElement("script");
+  s1.async = true;
+  s1.src = "https://www.googletagmanager.com/gtag/js?id=" + id;
+  document.head.appendChild(s1);
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);} 
+  window.gtag = gtag;
+  gtag("js", new Date());
+  gtag("config", id);
+})();
